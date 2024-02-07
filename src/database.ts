6aa1e859bcf83fdeeb56463d33394d2412955cda
Ivan A. Ferreira Aguiar <ivnagst@gmail.com>
2024-02-07 20:40:54 -0300

    chore: add database setup with knex

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import knex from 'knex';
+import knex, { Knex } from 'knex';
 
-export const knexSetup = knex({
+export const config: Knex.Config = {
 	client: 'sqlite',
 	connection: {
-		filename: './tmp/app.db',
+		filename: './db/app.db',
 	},
-});
+	useNullAsDefault: true,
+	migrations: {
+		extension: 'ts',
+		directory: './db/migrations',
+	},
+};
+
+export const knexSetup = knex(config);
